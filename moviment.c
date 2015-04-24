531a411d70ae54ae406d72f03cbb1eb2972cbc9b
danbastos <d.bastos@ua.pt>
2015-04-24 19:48:49 +0100

    added Moviment

--- moviment.c
+++ moviment.c
@@ -1,1 +1,32 @@
 #include "moviment.h" 
+
+void moviment_rotate90right()
+{
+	motors_rotateRight();
+	while(sensors_get() != RIGHT);
+	while(sensors_get() != ON_TRACK);
+}
+
+void moviment_rotate90Left()
+{
+	motors_rotateLeft();
+	while(sensors_get() != LEFT);
+	while(sensors_get() != ON_TRACK);
+}
+
+void moviment_forward()
+{
+	if(sensors_get() == ON_TRACK)
+		motors_forward();
+	else if (sensors_get() == RIGHT_TILTED)
+		motors_forwardLeft();
+	else if (sensors_get() == LEFT_TILTED)
+		motors_forwardRight();
+}
+
+void moviment_rotate180()
+{
+	motors_rotateRight();
+	while(sensors_get() != RIGHT);
+	while(sensors_get() != ON_TRACK);
+}
--- moviment.h
+++ moviment.h
@@ -1,6 +1,9 @@
 #ifndef MOVIMENT_H
 #define MOVIMENT_H
 
-
+void moviment_rotate90right();
+void moviment_rotate90left();
+void moviment_forward();
+void moviment_rotate180();
 
 #endif
